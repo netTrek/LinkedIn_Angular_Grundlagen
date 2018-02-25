499c78940c79d629fb59610cbe2dda268a825a8d
netTrek <us@netTrek.de>
2018-02-25 14:07:34 +0100

    04_02_Komponenten_anlegen_und_deklarieren

---
+++ src/app/user/user-list/user-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ( {
+  selector: 'in-user-list',
+  template: ''
+})
+export class UserListComponent {
+  constructor () {
+    console.log ( 'Hello UserList' );
+  }
+}
