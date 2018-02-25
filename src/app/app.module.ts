1acac15f2edde42e84bb40c8ba26d10b3684937d
netTrek <us@netTrek.de>
2018-02-25 13:07:15 +0100

    02_05_Modul_mit_Komponenten_importieren

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserModule } from './user/user.module';
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
