619ee7e587a87278436fa4a8d2da33f0d63600ed
netTrek <us@netTrek.de>
2018-02-25 15:25:40 +0100

    04_07_Komponenten_ueber_CLI

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user/user.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { UserNameComponent } from './user-name/user-name.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UserComponent, UserListComponent],
+  declarations: [UserComponent, UserListComponent, UserNameComponent],
   exports: [UserComponent, UserListComponent]
 })
 export class UserModule { }
