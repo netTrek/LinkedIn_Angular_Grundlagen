9ce984902feeaa87b286fe23c4278461d8ac2ff4
netTrek <us@netTrek.de>
2018-02-25 14:31:05 +0100

    04_04_Komponenten-Styles

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component ( {
   selector: 'in-user-list',
-  templateUrl: './user-list.component.html'
+  templateUrl: './user-list.component.html',
+  styleUrls: [ './user-list.component.scss' ]
 })
 export class UserListComponent {
   constructor () {
