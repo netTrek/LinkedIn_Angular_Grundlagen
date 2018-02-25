dbfc88dab4f87d9dc9fbf1a7201e7be6157e3809
netTrek <us@netTrek.de>
2018-02-25 15:17:28 +0100

    04_06_Encapsulation-Modi

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component ( {
   selector: 'in-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: [ './user-list.component.scss' ]
+  styleUrls: [ './user-list.component.scss' ],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class UserListComponent {
   constructor () {
