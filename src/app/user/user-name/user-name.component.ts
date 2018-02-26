68759728d82dbd4c9b3084557f14e581587b1dda
netTrek <us@netTrek.de>
2018-02-26 13:29:54 +0100

    06_05_Styles_binden

--- src/app/user/user-name/user-name.component.ts
+++ src/app/user/user-name/user-name.component.ts
@@ -1,27 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'in-user-name',
+@Component ( {
+  selector   : 'in-user-name',
   templateUrl: './user-name.component.html',
-  styleUrls: ['./user-name.component.scss']
-})
+  styleUrls  : [ './user-name.component.scss' ]
+} )
 export class UserNameComponent implements OnInit {
-  get name (): string {
-    return this._name; // `Herr ${this._name}`;
+
+  fontColor = 'red';
+  name      = 'Saban Ünlü';
+
+  constructor () {
   }
 
-  set name ( value: string ) {
-    this._name = value;
-  }
-
-  private _name = 'Saban Ünlü';
-
-  constructor() { }
-  ngOnInit() {  }
-
-  getName (): string {
-    console.log ( 'return name' , this.name );
-    return this._name;
+  ngOnInit () {
   }
 
 }
